0d87b2507d51ae4e4ed412aca0e695de1ad01246
Artem Pikulin <pikulin_ayu20@mail.ru>
2020-10-30 21:37:38 +0300

    14th_c++

---
+++ Practice/14/C++/14th/14th/14th.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    long long n;
+    int x;
+    std::cout << "Введите целое число\n";
+    std::cin >> n;
+    x = 1;
+    if (n == 0) {
+        std::cout << 0;
+        return 0;
+    }
+    while (n > 1) {
+        n = n / 2;
+        x = x + 1;
+    }
+    std::cout << x;
+}
