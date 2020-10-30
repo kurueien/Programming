391cbad49069371306e05bf3fccfe5b38a438e65
Artem Pikulin <pikulin_ayu20@mail.ru>
2020-10-30 17:34:16 +0300

    11th_c++

---
+++ Practice/11/C++/11th/11th/11th.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	std::cout << "Введите число и введите целое число-степень\n";
+	double x, s;
+	int y;
+	std::cin >> x >> y;
+	s = x;
+	for (int i = 1; i < y; i++)
+		s = s * x;
+	std::cout << x << " в степени " << y << " = " << s;
+		
+}
+
